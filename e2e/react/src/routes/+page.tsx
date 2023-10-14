10a30ffa0d1ae3215f2b3809ac4c07e7ec2fad66
Alec Aivazis <alec@aivazis.com>
2023-10-14 12:46:14 -0700

    move component fields out of root of e2e tests

--- e2e/react/src/routes/+page.tsx
+++ e2e/react/src/routes/+page.tsx
@@ -1,20 +1,9 @@
-import SponsorInfo from '~/components/SponsorInfo'
-
 import type { PageProps } from './$types'
 
 export default function ({ SponsorList }: PageProps) {
 	return (
 		<>
 			<h1>Houdini's React Interation tests</h1>
-			<div>
-				{SponsorList.sponsors.map((sponsor) => {
-					return (
-						<div key={sponsor.name}>
-							<SponsorInfo sponsor={sponsor} />
-						</div>
-					)
-				})}
-			</div>
 			<p>This is the HOME page. 🫵 can navigate with links 👇</p>
 		</>
 	)
